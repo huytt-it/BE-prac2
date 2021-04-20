900b85ebb4bcc7334ce1fc81138ac67795265cd5
byube1 <82696056+huytt-it@users.noreply.github.com>
2021-04-20 18:55:19 +0700

    fix: text commit

--- BE-prac2/Startup.cs
+++ BE-prac2/Startup.cs
@@ -33,7 +33,7 @@ namespace BE_prac2
             services.AddBusinessServices();
             services.ConfigJwt(Configuration);
             services.ConfigCors();
-            services.AddControllers();
+            services.AddControllers();    
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
